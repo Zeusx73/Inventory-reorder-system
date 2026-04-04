b70dc7c27e2be05e98954dd29c0835a35d928ff2
Zeusx73 <akashakashay14@gmail.com>
2026-04-04 07:52:16 +0000

    remove postgres checkpointer

--- backend/database.py
+++ backend/database.py
@@ -1,14 +1,10 @@
-import os
+from langgraph.checkpoint.memory import MemorySaver
 from contextlib import contextmanager
-from langgraph.checkpoint.postgres import PostgresSaver
-from dotenv import load_dotenv
-
-load_dotenv()
-
-DB_URL=os.getenv("POSTGRES_URL")
 
 @contextmanager
 def get_checkpointer():
-    with PostgresSaver.from_conn_string(DB_URL) as checkpointer:
-        checkpointer.setup()
+    checkpointer = MemorySaver()
+    try:
         yield checkpointer
+    finally:
+        pass
