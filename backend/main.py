5919c419ae7c9997213f77355ffe364a30983429
akashakashay14 <akashakashay14@gmail.com>
2026-04-15 07:53:10 +0000

    Your commit message

--- backend/main.py
+++ backend/main.py
@@ -20,7 +20,7 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=["https://inventory-reorder-system-production-ef47.up.railway.app"],
     allow_credentials=False,
     allow_methods=["*"],
     allow_headers=["*"],
